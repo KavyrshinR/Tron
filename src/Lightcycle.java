535d614c8d92fb93d88592c5c328cd6422def987
Roman <rom.kawirshin@gmail.com>
2015-10-13 22:29:48 +0300

    Init commit

---
+++ src/Lightcycle.java
@@ -0,0 +1,48 @@
+import java.awt.Color;
+
+class Lightcycle {
+    int[] coordX = new int[10000];
+    int[] coordY = new int[10000];
+    Color color;
+    int len = 1;
+    int dir = 0;
+	
+    public Lightcycle(int x, int y, int r, int g, int b) {
+        this.color = new Color(r, g, b);
+        coordX[0] = x; coordY[0] = y;
+    }
+	
+    boolean isAlive(Lightcycle enemy) {
+        for(int i = 1; i < len; i++) {
+            if((coordX[0] == coordX[i]) && (coordY[0] == coordY[i])) { 
+                return false;
+            }
+        }
+
+        for(int i = 0; i < enemy.len; i++) {
+            if((coordX[0] == enemy.coordX[i]) && (coordY[0] == enemy.coordY[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    public void move(int d) {
+
+        for(int i = len; i > 0; i--) {
+                coordX[i] = coordX[i - 1];
+        }
+
+        for(int i = len; i > 0; i--) {
+                coordY[i] = coordY[i - 1];
+        }
+
+        if(d == 0) {if(coordY[0] == 0){coordY[0] = MyTronGame.Height - 1;} else coordY[0] -= 1;}
+        if(d == 1) {if(coordY[0] == MyTronGame.Height - 1){coordY[0] = 0;} else coordY[0] += 1;}
+        if(d == 2) {if(coordX[0] == 0){coordX[0] = MyTronGame.Width - 1;} else coordX[0] -= 1;}
+        if(d == 3) {if(coordX[0] == MyTronGame.Width - 1){coordX[0] = 0;} else coordX[0] += 1;}
+        len++;
+    }
+}
+// LoooooooooooooooooooL
---
+++ src/Tron.java
@@ -0,0 +1,108 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Tron {
+    public static void main(String [] args) {
+        MyTronGame mtg = new MyTronGame();
+        mtg.go();
+    }
+}
+
+class MyTronGame {
+    public static int Scale = 8;
+    public static int Width = 84;
+    public static int Height = 84;
+    JFrame frame = new JFrame("TronGame");
+    GamePanel gp;
+    Lightcycle moto1;
+    Lightcycle moto2;
+    Color backColor = new Color(80, 102, 80);
+    Color sharpColor = new Color(51, 9, 221);
+	
+    public void go() {
+        gp = new GamePanel();
+        gp.addMouseListener(new MouseListen());
+        gp.addKeyListener(new KeyListen());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, gp);
+        frame.setSize(Width * Scale + 6, Height * Scale + 28);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        moto1 = new Lightcycle(20, 40, 255, 82, 82);
+        moto2 = new Lightcycle(70, 40, 200, 0, 100);
+
+        while(moto1.isAlive(moto2) && moto2.isAlive(moto1)) {
+            moto1.move(moto1.dir);
+            moto2.move(moto2.dir);
+            gp.repaint();
+            try {
+                Thread.sleep(60);
+            } catch(Exception ex) {}
+        }
+    }
+	
+	public void startNewGame() {
+		moto1 = new Lightcycle(20, 40, 255, 82, 82);
+		moto2 = new Lightcycle(70, 40, 200, 0, 100);
+		this.go();
+	}
+	
+    class GamePanel extends JPanel {
+        @Override
+        public void paintComponent(Graphics g) {
+            
+            g.setColor(backColor);
+            g.fillRect(0, 0, Width * Scale, Height * Scale);
+            
+            g.setColor(sharpColor);
+            for(int i = 0; i < Width * Scale; i += Scale * 3) {
+                g.drawLine(i, 0, i, Width * Scale);
+            }
+            
+            for(int i = 0; i < Height * Scale; i += Scale * 3) {
+                g.drawLine(0, i, Height * Scale, i);
+            }
+
+            g.setColor(moto1.color);
+            for(int i = 0; i < moto1.len; i++) {
+                g.fillRect(moto1.coordX[i] * Scale, moto1.coordY[i] * Scale, Scale - 1, Scale - 1);
+            }
+            
+            g.setColor(moto2.color);
+            for(int i = 0; i < moto2.len; i++) {
+                g.fillRect(moto2.coordX[i] * Scale, moto2.coordY[i] * Scale, Scale - 1, Scale - 1);
+            }
+        }
+    }
+	
+    class MouseListen implements MouseListener {
+        public void mouseClicked(MouseEvent e) {}
+        public void mouseEntered(MouseEvent e) {}
+        public void mouseExited(MouseEvent e) {}
+        public void mousePressed(MouseEvent e) {}
+
+        public void mouseReleased(MouseEvent e) {
+			//startNewGame();
+            gp.requestFocus(true);
+        }
+    }
+	
+    class KeyListen implements KeyListener {
+        public void keyPressed(KeyEvent e) {
+            if(e.getKeyCode() == KeyEvent.VK_UP && moto2.dir != 1) {moto2.dir = 0;}
+            if(e.getKeyCode() == KeyEvent.VK_DOWN && moto2.dir != 0) {moto2.dir = 1;}
+            if(e.getKeyCode() == KeyEvent.VK_LEFT && moto2.dir != 3) {moto2.dir = 2;}
+            if(e.getKeyCode() == KeyEvent.VK_RIGHT && moto2.dir != 2) {moto2.dir = 3;}
+            
+            if(e.getKeyCode() == KeyEvent.VK_W && moto1.dir != 1) {moto1.dir = 0;}
+            if(e.getKeyCode() == KeyEvent.VK_S && moto1.dir != 0) {moto1.dir = 1;}
+            if(e.getKeyCode() == KeyEvent.VK_A && moto1.dir != 3) {moto1.dir = 2;}
+            if(e.getKeyCode() == KeyEvent.VK_D && moto1.dir != 2) {moto1.dir = 3;}
+        }
+		
+        public void keyReleased(KeyEvent e) {}
+		public void keyTyped(KeyEvent e) {}	
+    }
+}
+// LoooooooooooooooooooL
